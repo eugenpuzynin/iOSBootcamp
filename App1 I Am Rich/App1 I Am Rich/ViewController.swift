717174a2c8fa7f4c51e0b90882f9e9d118a6110f
Eugen <101580765+eugenpuzynin@users.noreply.github.com>
2022-03-16 23:07:54 +0100

    Add App 1: I Am Rich

---
+++ App1 I Am Rich/App1 I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App1 I Am Rich
+//
+//  Created by Eugen Puzynin on 16.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
