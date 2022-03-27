fc6cbae2dd54928a73bb92dab520fb73d1256a62
Eugen <101580765+eugenpuzynin@users.noreply.github.com>
2022-03-27 21:52:28 +0200

    Add App 11: Todoey

---
+++ App11 Todoey/App11 Todoey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App11 Todoey
+//
+//  Created by Eugen Puzynin on 27.03.22.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
